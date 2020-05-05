7fea7eb13d4400f1d9a95bd19c35d905da79d12f
guptaaniket <gupta.aniket5828@gmail.com>
2020-05-05 19:40:09 +0530

    Updates

---
+++ precache-manifest.72f1061417378dca3eaebc93b42f6e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb42daf89f630434c256306247dbbd85",
+    "url": "/react-draganddrop/index.html"
+  },
+  {
+    "revision": "a15dcb90ae73fd8502d1",
+    "url": "/react-draganddrop/static/css/main.192250aa.chunk.css"
+  },
+  {
+    "revision": "e1ff37dc7124548b628a",
+    "url": "/react-draganddrop/static/js/2.699bcaf3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-draganddrop/static/js/2.699bcaf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a15dcb90ae73fd8502d1",
+    "url": "/react-draganddrop/static/js/main.80e1e50c.chunk.js"
+  },
+  {
+    "revision": "959e04013f03a416abb9",
+    "url": "/react-draganddrop/static/js/runtime-main.5865c906.js"
+  }
+]);
